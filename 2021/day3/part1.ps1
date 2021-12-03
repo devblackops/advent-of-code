53cb07601939c2ba714673e7bed68d72ec998ae7
Brandon Olin <bolin@stackoverflow.com>
2021-12-03 00:09:43 -0800

    optimize 2021/day3/part1

--- 2021/day3/part1.ps1
+++ 2021/day3/part1.ps1
@@ -1,34 +1,11 @@
-function Get-MostCommon {
-    param(
-        [string[]]$Data,
-        [int]$Bit,
-        [string]$Type
-    )
-
-    $ones  = $Data.Where({[int][string][char[]]$_[$Bit] -eq 1}).Count
-    $zeros = $Data.Count - $ones
-
-    if ($Type -eq 'gamma') {
-        if ($ones -ge $zeros) {
-            '1'
-        } else {
-            '0'
-        }
-    } elseIf ($Type -eq 'epsilon') {
-        if ($zeros -le $ones) {
-            '0'
-        } else {
-            '1'
-        }
-    }
-}
-
 $data = Get-Content ./input.txt
 $gammaBits = $epsilonBits = ''
 for ($x = 0; $x -lt $data[0].Length; $x++) {
-    $gammaBits   += Get-MostCommon $data $x 'gamma'
-    $epsilonBits += Get-MostCommon $data $x 'epsilon'
+    $ones  = $data.Where({[string][char[]]$_[$x] -eq '1'}).Count
+    $zeros = $data.Count - $ones
+    $gammaBits   += ($ones -ge $zeros ? '1' : '0')
+    $epsilonBits += ($zeros -le $ones ? '0' : '1')
 }
-$gamma = [Convert]::ToInt32([string]$gammaBits,2)
-$epsilon = [Convert]::ToInt32([string]$epsilonBits,2)
+$gamma = [Convert]::ToInt32($gammaBits, 2)
+$epsilon = [Convert]::ToInt32($epsilonBits, 2)
 $gamma * $epsilon
